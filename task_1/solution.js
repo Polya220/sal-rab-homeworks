49f00a9d78765927bc9778f878f4a53d4a2c849d
Polina <eparis@atlassian.com>
2023-02-21 13:24:23 +0500

    task 1.3 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -55,7 +55,11 @@ function addInCartSumChange(sum, delta) {
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
     // замените склеивание, используемое в newSum Text на шаблонную строку
-
+    let newSum;
+    newSum = oldSum;
+    newSum = newSum + difference;
+    let newSumText =  newSum + ' ₽';
+    newSumText = `${newSum} ₽`;
     // Конец решения задания №1.3.
 
     return newSumText;
