e09a18e87b88ca3871cfbc43ab6d0beb06c1cb77
Polina <eparis@atlassian.com>
2023-03-01 13:47:21 +0500

    task 4 -change

--- task_4/solution.js
+++ task_4/solution.js
@@ -21,12 +21,16 @@ function parseProducts(json) {
 // Напишите цикл, в котором перебираете все products от 0 до (length - 1)
 // и выполняете addProduct с каждым элементом массива products (передавая элемент массива как аргумент)
 
+function parseProducts(json) {
+	let data = JSON.parse(json);
+	return data.products;
+}
+
 function renderProductsCards(json) {
-    clearProducts();
-      let products = parseProducts(json); 
-return products;
-      let lenght = products.length; {
-      for (let i = 0; i < length; i += 1) 
-        addProduct(products[i]);
-  }
+	clearProducts();
+	let products = parseProducts(json);
+	let length = products.length;
+	for (let i = 0; i < (length - 1); i += 1) {
+		addProduct(products[i]);
+	}
 }
