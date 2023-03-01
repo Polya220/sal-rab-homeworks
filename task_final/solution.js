582a51021e0011ff29c43b43f3d4c8a4a7ec3b30
Polina <eparis@atlassian.com>
2023-03-01 14:21:27 +0500

    task final change

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,24 +34,29 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {order: {address, sum}, client: {name, phone}, goods:[]};;
-
-    let countOfGoods = goods.length;
-
-    for (let i = 0; i < countOfGoods; i+=1) {
-        let goodsData = {
-            title: goods[i].title,
-            count: goods[i].count
-        }; 
-    data.goods.push({title:goods[i].title, count:goods[i].count});
-    }
-
-    data.order.address = "ул. "`${address.street}` + ", дом " + `${address.house}` + ", " + `${address.entrance}` + ", подъезд, " + `${address.floor}` + " этаж, кв. " + `${address.flat}`;
-    data.order.sum = sum;
-
-    data.client = `${name}` + " " + `${phone}`;
-
-    let jsonData = JSON.stringify({data:data});
-
-    return jsonData;
+	let data = {
+		order: {
+			address,
+			sum
+		},
+		client: {
+			name,
+			phone
+		},
+		goods: []
+	};;
+	let countOfGoods = goods.length;
+	for (let i = 0; i < countOfGoods; i++) {
+		data.goods.push({
+			title: goods[i].title,
+			count: goods[i].count
+		})
+	}
+	data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв. ${address.flat}`;
+	data.order.sum = sum;
+	data.client = `${name} ${phone}`;
+	let jsonData = JSON.stringify({
+		data
+	});
+	return jsonData;
 }
